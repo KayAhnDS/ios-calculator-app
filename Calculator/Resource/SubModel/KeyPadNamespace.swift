ce1a3213b565d78a737db73f0ecc5e5118300808
KayAhnDS <dkseotn123@gmail.com>
2022-06-04 15:55:39 +0900

    feat: keypad 네임스페이스 추가

---
+++ Calculator/Resource/SubModel/KeyPadNamespace.swift
@@ -0,0 +1,27 @@
+//
+//  KeyPadNamespace.swift
+//  Calculator
+//
+//  Created by Kay on 2022/06/04.
+//
+
+import Foundation
+
+struct KeyPadNamespace {
+    
+    //MARK: - Number
+    static let zero: String = "0"
+    static let doubleZero: String = "00"
+    static let one: String = "1"
+    static let two: String = "2"
+    static let three: String = "3"
+    static let four: String = "4"
+    static let five: String = "5"
+    static let six: String = "6"
+    static let seven: String = "7"
+    static let eight: String = "8"
+    static let nine: String = "9"
+    static let dot: String = "."
+    
+    private init() { }
+}
